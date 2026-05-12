58ee8b5751914f0f1bceca67b62114c134bcaf05
al460576 <al460576@uji.es>
2026-05-12 19:02:12 +0200

    Error En el Layer1
    Error encontrado en Layer1 que hace que el personaje no se mueva horizontalmente

--- Assets/Sprites/Jugar.cs
+++ Assets/Sprites/Jugar.cs
@@ -2,10 +2,18 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
-public class Principal : MonoBehaviour
+public class Jugar : MonoBehaviour
 {
     public void CambiarPantalla(string nombre){
+        Time.timeScale = 1f;
         SceneManager.LoadScene(nombre);
+    }
+    public void VolverAlJuego()
+    {
+        PlayerMove playerMove = FindObjectOfType<PlayerMove>();
+        if (playerMove != null)        {
+            playerMove.CloseOptions();
+        }
     }
 
     public void Salir(){
--- Assets/Sprites/PlayerMove.cs
+++ Assets/Sprites/PlayerMove.cs
@@ -36,17 +36,21 @@ public class PlayerMove : MonoBehaviour
             }
         }
         if (!isPaused){
-            if(Input.GetKey(KeyCode.RightArrow)||Input.GetKey(KeyCode.D)){
+             Debug.Log("inputX: " + inputX + " | D: " + Input.GetKey(KeyCode.D) + " | RightArrow: " + Input.GetKey(KeyCode.RightArrow));
+            if(Input.GetKey(KeyCode.RightArrow)||Input.GetKey(KeyCode.D))
+            {
                 inputX = velocidad;
-            }else if(Input.GetKey(KeyCode.LeftArrow)||Input.GetKey(KeyCode.A)){
+            }else if(Input.GetKey(KeyCode.LeftArrow)||Input.GetKey(KeyCode.A)) 
+            {
                 inputX = -velocidad;
+            }else{
+                inputX = 0;
             }
-
-            if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) && checkGround.isGrounded)
+            if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)|| Input.GetKeyDown(KeyCode.Space)) && checkGround.isGrounded)
             {
                 jumpRequested = true;
             }
-            jumpHeld = Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W);    
+            jumpHeld = Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.Space);    
         }else{
             inputX = 0;
             jumpHeld = false;
@@ -55,14 +59,15 @@ public class PlayerMove : MonoBehaviour
     
     void FixedUpdate()
     {
-        if (!isPaused) return;
+        if (isPaused) return;
 
         rb2D.linearVelocity = new Vector2(inputX, rb2D.linearVelocity.y);
+         Debug.Log("Velocidad aplicada: " + rb2D.linearVelocity + " | inputX: " + inputX);
 
         if (jumpRequested)
         {
             if (checkGround.isGrounded)
-                rb2D.linearVelocity = new Vector2(rb2D.linearVelocity.x, salto);
+                rb2D.AddForce(Vector2.up * salto, ForceMode2D.Impulse);
             jumpRequested = false;
         }
 
@@ -70,19 +75,19 @@ public class PlayerMove : MonoBehaviour
         {
             if (rb2D.linearVelocity.y < 0)
                 rb2D.linearVelocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
-            else if (rb2D.linearVelocity.y > 0 && (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)))
+            else if (rb2D.linearVelocity.y > 0 && jumpHeld)
                 rb2D.linearVelocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
         }
     }
 
-    void OpenOptions()
+    public void OpenOptions()
     {
         isPaused = true;
         Time.timeScale = 0f;
         SceneManager.LoadScene("Opciones", LoadSceneMode.Additive);
     }
 
-    void CloseOptions()
+    public void CloseOptions()
     {
         isPaused = false;
         Time.timeScale = 1f;
